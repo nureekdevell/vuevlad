6ff40340ca115a5af1e234254ccbcf136a76092a
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-02-14 22:55:49 +0500

    initial

---
+++ vue-project/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+	<div>
+		<input
+			v-model="inputValue"
+			@keypress.enter="addNotes"
+			type="text"
+			placeholder="nimadur"
+		/>
+		<span v-for="(note, i) in notes" key="note">
+			<li>
+				{{ note }} {{ i + 1 }}
+				<button class="" @click="remove(i)">Delete</button>
+			</li></span
+		>
+		<h1
+			:style="{
+				color: inputValue.length <= 3 ? 'darkred' : 'darkblue',
+			}"
+		>
+			Ismingiz 3 ta harfdan kop bolsin {{ double }}
+		</h1>
+		<button @click="increment">increment</button>
+	</div>
+</template>
+
+<script>
+export default {
+	// data
+	data() {
+		return {
+			count: 0,
+			notes: [],
+			inputValue: '',
+		};
+	},
+	// method
+	methods: {
+		increment() {
+			this.count++;
+		},
+		addNotes() {
+			if (this.inputValue !== '') {
+				this.notes.push(this.inputValue);
+				this.inputValue = '';
+			}
+		},
+
+		remove(i) {
+			this.notes.splice(i, 1);
+		},
+	},
+	computed: {
+		// addNotes() {
+		// 	console.log('double');
+		// 	if (this.inputValue !== '') {
+		// 		this.notes.push(this.inputValue);
+		// 		this.inputValue = '';
+		// 	}
+		// },
+	},
+	watch: {
+		inputValue(value) {
+			if (value.length > 15) {
+				this.inputValue = ' ';
+			}
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
