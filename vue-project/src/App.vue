bd7d4535335863c8f38ba2d1f80da9f7d90d0c48
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-02-17 23:14:46 +0500

    initial

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,56 +1,35 @@
 <template>
-	<div>
-		<input
-			v-model="inputValue"
-			@keypress.enter="addNotes"
-			type="text"
-			placeholder="nimadur"
-		/>
-		<span v-for="(note, i) in notes" key="note">
-			<li>
-				{{ note }} {{ i + 1 }}
-				<button class="" @click="remove(i)">Delete</button>
-			</li></span
-		>
-		<h1
-			:style="{
-				color: inputValue.length <= 3 ? 'darkred' : 'darkblue',
-			}"
-		>
-			Ismingiz 3 ta harfdan kop bolsin {{ double }}
-		</h1>
-		<button @click="increment">increment</button>
-	</div>
+	<input v-model.lazy="firstName" type="text" />
+	<p>{{ firstName }}</p>
+	<br />
+	<p>||||</p>
+	<input v-model.trim="lastName" type="text" />
+	<p>{{ lastName }}</p>
+
+	<p>{{ visited.join(', ') }}</p>
+	<select multiple v-model="visited">
+		<option value="">Please select your Country</option>
+		<option value="US">US</option>
+		<option value="UZ">UZ</option>
+		<option value="KZ">KZ</option>
+	</select>
 </template>
 
 <script>
-import { createApp, handleError } from 'vue';
+import { createApp } from 'vue';
 
 export default {
 	// data
 	data() {
 		return {
-			count: 0,
-			notes: [],
-			inputValue: '',
+			firstName: '',
+			lastName: '',
+			country: '',
+			visited: [],
 		};
 	},
 	// method
-	methods: {
-		increment() {
-			this.count++;
-		},
-		addNotes() {
-			if (this.inputValue !== '') {
-				this.notes.push(this.inputValue);
-				this.inputValue = '';
-			}
-		},
-
-		remove(i) {
-			this.notes.splice(i, 1);
-		},
-	},
+	methods: {},
 
 	beforeCreate() {
 		console.log('beforCreate 1 ');
