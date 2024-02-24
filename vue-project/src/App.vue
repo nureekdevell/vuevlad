cda1ec01afc6f2bbe826719f35f411d8c044c9ec
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-02-24 22:56:25 +0500

    initial

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,82 +1,26 @@
 <template>
-	<input v-model.lazy="firstName" type="text" />
-	<p>{{ firstName }}</p>
-	<br />
-	<p>||||</p>
-	<input v-model.trim="lastName" type="text" />
-	<p>{{ lastName }}</p>
-
-	<p>{{ visited.join(', ') }}</p>
-	<select multiple v-model="visited">
-		<option value="">Please select your Country</option>
-		<option value="US">US</option>
-		<option value="UZ">UZ</option>
-		<option value="KZ">KZ</option>
-	</select>
+	<div>
+		<List :peaple="peaple" @message="displayMessage" />
+	</div>
 </template>
 
 <script>
-import { createApp } from 'vue';
-
+import List from './components/List.vue';
 export default {
-	// data
+	components: {
+		List,
+	},
 	data() {
 		return {
-			firstName: '',
-			lastName: '',
-			country: '',
-			visited: [],
+			peaple: ['Max', 'Jack', 'Leo'],
 		};
 	},
-	// method
-	methods: {},
-
-	beforeCreate() {
-		console.log('beforCreate 1 ');
-	},
-
-	created() {
-		console.log('Created');
-	},
-
-	beforeMount() {
-		console.log('beforeMount');
-	},
-	mounted() {
-		console.log('mounted');
-	},
-	beforeUpdate() {
-		console.log('beforeUpdate');
-	},
-	updated() {
-		console.log('updated');
-	},
-
-	beforeUnmount() {
-		console.log('beforeUnmounted');
-	},
-
-	unmounted() {
-		console.log('uchdi ');
-	},
-
-	computed: {
-		// addNotes() {
-		// 	console.log('double');
-		// 	if (this.inputValue !== '') {
-		// 		this.notes.push(this.inputValue);
-		// 		this.inputValue = '';
-		// 	}
-		// },
-	},
-	watch: {
-		inputValue(value) {
-			if (value.length > 15) {
-				this.inputValue = ' ';
-			}
+	methods: {
+		displayMessage: function (message) {
+			this.peaple.push(message);
 		},
 	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
---
+++ vue-project/src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<input type="text" v-model="message" @keyup.enter="saveMessage" />
+		<h2>Inner text</h2>
+		<ul>
+			<li v-for="person in peaple" :key="person">{{ person }}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		peaple: {
+			type: Array,
+			reuired: true,
+		},
+	},
+	data() {
+		return {
+			message: '',
+		};
+	},
+	methods: {
+		saveMessage: function () {
+			this.$emit('message', this.message);
+			this.message = '';
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
